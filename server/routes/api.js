5d23550f1de91b08e28f653c2f64d3b719a5c3af
Hamdi Chebbi <hamdi.chebbi@outlook.fr>
2017-10-01 19:29:42 +0100

    Get Posts
    get all posts with axios

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
+
 const axios = require('axios');
+const API = 'https://jsonplaceholder.typicode.com';
 
 // GET API LISTENING
 
-router.get('/',(req,res) => {
+router.get('/', (req, res) => {
   res.send('API Works ! nice :)')
+});
+
+// get all posts
+router.get('/posts', (req, res) => {
+  axios.get(`${API}/posts`)
+  .then(posts => {
+    res.status(200).json(posts.data);
+  })
+  .catch(error => {
+    res.status(500).send(error)
+  });
 });
 
 module.exports = router;
